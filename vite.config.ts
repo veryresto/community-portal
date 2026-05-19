3d096bab826b6db976ac0ea1870e1ad4a8ea54a0
veryresto <veryresto@gmail.com>
2026-05-19 18:39:30 +0700

    feat: configure dev server host and implement session-based SSO redirection with expanded allowed origins

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: 5173,
+    allowedHosts: [
+      "community.lvh.me",
+      "localhost"
+    ]
+  },
   plugins: [react()],
 })
