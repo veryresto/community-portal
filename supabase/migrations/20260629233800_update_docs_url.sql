6e9a258bac076a57ff795b96b5009d1ac590f017
veryresto <veryresto@gmail.com>
2026-06-30 00:15:49 +0700

    chore: update documents application URL in public.applications table

---
+++ supabase/migrations/20260629233800_update_docs_url.sql
@@ -0,0 +1,6 @@
+-- Migration: Update documents application URL in public.applications table
+-- Target file: /Users/a/Codes/community-platform/supabase/migrations/20260629233800_update_docs_url.sql
+
+UPDATE public.applications
+SET url = 'http://docs.localtest.me:3000'
+WHERE slug = 'documents';
